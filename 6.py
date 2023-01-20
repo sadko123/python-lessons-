d21e2de59f006a13cbc52ca06bc2073854c70c88
sadko123 <sad_ko0@list.ru>
2023-01-20 16:15:29 +0600

    change

--- 6.py
+++ 6.py
@@ -1,24 +1,28 @@
 f = open('//book/!-Буфер/Пб/Задачи/ГИТ/Задание 2/Perepis.txt', 'r')
 
-l = [elem for elem in f]
+
 count = 0
 
-for i in l:
+for i in f:
     s = i.split()
     if int(s[3][6:]) < 1978:
         print(s[0], s[1], s[2])
         count += 1
 print(count)
 
-    
-        
-    
+f.close()
+
+
+f = open('//book/!-Буфер/Пб/Задачи/ГИТ/Задание 2/Perepis.txt', 'r')
 a = int(input('С какого года'))
 b = int(input('До какого года'))
 
-for i in l:
+for i in f:
+    s = i.split()
     if (int(s[3][6:]) > a) and (int(s[3][6:]) < b):
         print(s[0], s[1], s[2])
-        
 
-    
+f.close()
+
+
+
